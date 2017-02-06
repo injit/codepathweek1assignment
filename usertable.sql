4a213d9654815248b0684f641d7eb19cf61e4370
injit <injit.gurung@gmail.com>
2017-02-05 23:59:43 -0500

    Update usertable.sql

--- usertable.sql
+++ usertable.sql
@@ -37,30 +37,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `username`, `created_at`) VALUES
 (1, 'Indrajit', 'Gurung', 'injit@gmail.com', 'injit', '2017-02-05 00:00:00'),
 (5, 'second', 'sss', 'fkejkfe', 'ooo', '2017-02-06 01:57:02'),
-(6, 'kkfekfe', 'wefew', '232', 'asdf', '2017-02-06 01:57:09'),
-(7, 'try', 'try', 'try', 'try', '2017-02-06 02:01:12'),
-(8, 'kkfekfe', 'wefew', '232', 'asdf', '2017-02-06 02:10:32'),
-(9, 'second', 'sss', 'fkejkfe', 'ooo', '2017-02-06 02:10:37'),
-(12, '', '', '', '', '2017-02-06 02:43:38'),
-(13, '', '', '', '', '2017-02-06 02:43:57'),
-(14, '', '', '', '', '2017-02-06 02:44:58'),
-(15, '', '', '', '', '2017-02-06 02:44:59'),
-(16, '', '', '', '', '2017-02-06 02:45:00'),
-(17, '', '', '', '', '2017-02-06 02:45:40'),
-(18, 'asdf', 'asdf', 'asdf', 'asdf', '2017-02-06 02:45:52'),
-(19, '', 'asdf', '', '', '2017-02-06 02:45:57'),
-(20, 'full', '', '', '', '2017-02-06 02:50:43'),
-(21, 'kekkeke', '', '', '', '2017-02-06 02:51:06'),
-(22, '', '', '', 'fdfd', '2017-02-06 02:54:51'),
-(23, 'kkfkef', 'fkkfek', 'faes', 'fefe', '2017-02-06 02:56:40'),
-(24, 'sdsd', 'mmmd', 'kckkdkc', 'qweecd', '2017-02-06 03:13:43'),
-(25, 'kkoioi', 'kkkkkki', 'kkkk@fdfdk.com', 'asdfllllll', '2017-02-06 03:26:08'),
-(26, 'fefe', 'adfdfd', 'asdf@ffef.com', 'kkllflelfle', '2017-02-06 03:26:41'),
-(27, 'fefe', 'af', 'asdf@ffef.com', 'kkllflelfle', '2017-02-06 03:26:52'),
-(28, 'fefe', 'af', 'asdfffe@f.com', 'kkllflelfle', '2017-02-06 03:27:04'),
-(29, 'dfdfdf', 'nk', 'mmfme@gmail.com', 'lllfefefefef', '2017-02-06 03:29:02'),
-(30, 'sdf', 'dfefe', 'fdfd@asdf.com', 'dfdffefefe', '2017-02-06 04:06:29'),
-(31, 'a', 'dd', 'mfd@gmail.com', 'kkfeffff', '2017-02-06 04:24:01');
+
 
 --
 -- Indexes for dumped tables
